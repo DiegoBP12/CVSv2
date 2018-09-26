bed67a578249d1f5ebc0af720030b199f38ec955
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2018-09-25 21:51:50 -0500

    Se declaran las variables en el model y el metodo de lectura

--- src/models/ModelCvs.java
+++ src/models/ModelCvs.java
@@ -4,11 +4,67 @@
  * and open the template in the editor.
  */
 package models;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import views.ViewCvs;
 /**
  *
  * @author Diego
  */
 public class ModelCvs {
+    private String nombre;
+    private String email;
+    private String path = "";
+    private String mensaje;
     
+    ViewCvs viewCvs;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    /**
+     *  Lee un archivo según la ruta especificada
+     * 
+     */
+    public void readFile(){
+        try{
+                String row; 
+            try (FileReader file = new FileReader(path); BufferedReader bufferedReader = new BufferedReader(file)){
+                while ((row = bufferedReader.readLine()) != null){    
+                        mensaje = mensaje + row + "\n";
+                }
+            } 
+        }catch (FileNotFoundException ex) {
+                JOptionPane.showMessageDialog(viewCvs,"No se econtró el archivo" + ex.getMessage());
+        } catch (IOException ex) {
+                JOptionPane.showMessageDialog(viewCvs,"Error en I/O operación" + ex.getMessage());
+        }
+    }
+
 }
