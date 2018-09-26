6a6770d992562664a02e209eb555e75369d4bc77
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2018-09-25 22:57:54 -0500

    Se creo los metodos del controlador, solo para imprimir los resultados

--- src/controllers/ControllerCvs.java
+++ src/controllers/ControllerCvs.java
@@ -4,11 +4,46 @@
  * and open the template in the editor.
  */
 package controllers;
-
+import views.ViewCvs;
+import models.ModelCvs;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Diego
  */
 public class ControllerCvs {
+    ViewCvs viewCvs;
+    ModelCvs modelCvs;
+    ActionListener ac = new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == viewCvs.jb_limpiar)
+                    limpiar();
+            else if(e.getSource() == viewCvs.jb_guardar){
+                
+            }
+        }
+    };
+    public ControllerCvs(ViewCvs viewCvs, ModelCvs modelCvs) {
+        this.viewCvs = viewCvs;
+        this.modelCvs = modelCvs;
+        this.viewCvs.jb_guardar.addActionListener(ac);
+        this.viewCvs.jb_limpiar.addActionListener(ac);
+    }
+    
+    public void limpiar(){
+        viewCvs.jtf_nombre.setText("");
+        viewCvs.jtf_email.setText("");
+    }
+    
+    private void initComponents(){
+        viewCvs.setVisible(true);
+        modelCvs.readFile();
+        viewCvs.jtf_nombre.setText(modelCvs.getNombre());
+        viewCvs.jtf_email.setText(modelCvs.getEmail());
+        
+    }
     
 }
