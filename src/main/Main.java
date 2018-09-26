270614e7f71511912f0ed1bc18fc8c1ecbd33435
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2018-09-25 23:02:23 -0500

    Se codifica el main para iniciar el programa

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package main;
-
+import views.ViewCvs;
+import models.ModelCvs;
+import controllers.ControllerCvs;
 /**
  *
  * @author Diego
@@ -15,7 +17,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        ViewCvs viewCvs = new ViewCvs();
+        ModelCvs modelCvs = new ModelCvs();
+        ControllerCvs controllerCvs = new ControllerCvs(viewCvs,modelCvs);
     }
     
 }
